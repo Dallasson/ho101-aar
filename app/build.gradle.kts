321eca471150e0f1a8ffac367178ae5d2f9a90f7
DESKTOP-JJ86S6U\BreezyWeezy <eddinetaki087@gmail.com>
2024-08-20 15:12:58 +0100

    new changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    //alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.sentry)
 }
